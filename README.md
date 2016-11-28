53c540308ebda249bdfa45d17154a7e571d13079
ZhangHaolei <debmzhang@163.com>
2016-11-28 16:55:21 +0800

    add some func

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-#### web 开发常用小工具
+web toolbox
+===========
+## 安装
+`composer require debmzhang/toolbox`
+## 使用
+```php
+require __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+use debmzhang\toolbox\Tool;
+
+$tool = new Tool;
+// 避免中文被转换成unicode
+echo $tool->json_encode2('欢迎使用 toolbox ^_^');
+// 生成手机短信校验码
+// int $type 0 纯字母 1 纯数字(默认) 2 混合
+// int $len 长度 默认6位
+$type = 1;
+$len = 6;
+echo $tool->generateSmsCode($type, $len);
+// 生成一个订单号
+// string $prefix 订单前缀, 默认空
+// string $connectors 订单前缀与数字串之间的连接符, 默认空
+// int $len 随机数位数, 默认8位
+$prefix = 'alipay';
+$connectors = '-';
+$len = 8;
+echo $tool->generateOrderNO($prefix, $connectors, $len);
+// 把秒转换成 x天x时x分x秒 的格式
+$seconds = 12345678;
+echo $tool->formatTime($seconds);
+// 验证是否是一个手机号码(是正确的手机返回 true)
+$phone = 13838333333;
+var_dump($tool->checkIsPhoneNumber($phone));
+// 验证是否是一个正确的身份证号码
+$idCard = 110101201501023656;
+var_dump($tool->checkIsIdentityCard($idCard));
+// 把结果以 json 格式返回
+// mixed $code 0:正常 大于0 都是非正常
+// mixed $msg
+// array $result
+// bool $isJsonp 是否使用 jsonp 方式提交数据
+// string $param jsonp 方式提交参数名
+$code = 0;
+$msg = 'success';
+$result = array(
+    'name' => 'debm',
+    'sex' => 'M',
+    'age' => '18',
+);
+return $tool->ajaxMessage($code, $msg, $result);
+
+// 更多功能等待添加...
+```
--- src/Tool.php
+++ src/Tool.php
@@ -32,39 +32,89 @@ class Tool
     }
 
     /**
-     * 生成字母+数字
+     * 生成手机短信校验码
      * 
-     * @param int $type 0 纯字母 1 带数字
+     * @param int $type 0 纯字母 1 纯数字 2 混合
+     * @param int $len 长度 默认6位
      */
-    public function generateLetters($type = 0, $min = 6, $max = 10)
+    public function generateSmsCode($type = 1, $len = 6)
     {
-        $letterStr = $letters = $numbers = '';
-        // 字符串长度
-        $len = mt_rand($min, $max);
+        $code = '';
+        // 纯字母
         if (0 == $type) {
-            for ($i = 0; $i <= $len; $i++) {
-                $letter = mt_rand(97, 122);
-                $letterStr .= chr($letter);
+            for ($i = 0; $i < $len; $i++) {
+                $ascii = mt_rand(97, 122);
+                $code .= chr($ascii);
             }
         }
+        // 纯数字
         if (1 == $type) {
-            // 字母长度
-            $letterLen = mt_rand(4, $len);
-            // 数字长度
-            $numberLen = $len - $letterLen;
-            // 字母字符串
-            for ($i = 0; $i <= $letterLen; $i++) {
-                $letter = mt_rand(97, 122);
-                $letters .= chr($letter);
+            for ($i = 0; $i < $len; $i++) {
+                $ascii = mt_rand(48, 57);
+                $code .= chr($ascii);
             }
-            // 数字字符串
-            for ($i = 0; $i <= $numberLen; $i++) {
-                $letter = mt_rand(48, 57);
-                $numbers .= chr($letter);
-            }
-            $letterStr = $letters . $numbers;
         }
-        return $letterStr;
+        // 混合
+        if (2 == $type) {
+            // 去掉易混淆的字母/数字
+            $chars = array('2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
+            for ($i = 0; $i < $len; $i++) {
+                $code .= $chars[mt_rand(0, 31)];
+            }
+        }
+        return $code;
+    }
+
+    /**
+     * 按年月日时分秒+随机数 生成一个订单
+     *
+     * @param string $prefix 订单前缀, 默认空
+     * @param string $connectors 订单前缀与数字串之间的连接符, 默认空
+     * @param int $len 随机数位数, 默认8位
+     */
+    public function generateOrderNO($prefix = '', $connectors = '', $len = 8)
+    {
+        $len = (int) $len;
+        if (!$len) {
+            $len = 8;
+        }
+        $datetimeStr = date('YmdHis');
+        $numberStr = '';
+        for ($i = 0; $i < $len; $i++) {
+            $ascii = mt_rand(48, 57);
+            $numberStr .= chr($ascii);
+        }
+        return $prefix . $connectors . $datetimeStr . $numberStr;
+    }
+
+    /**
+     * formatTime
+     * 把秒转换成 x天x时x分x秒 的格式
+     *
+     * @param  mixed $seconds
+     */
+    public function formatTime($seconds)
+    {
+        $seconds = intval($seconds);
+        $d = floor($seconds / 86400);
+        $left = $seconds % 86400;
+        $h = floor($left / 3600);
+        $left = $left % 3600;
+        $m = floor($left / 60);
+        $s = $left % 60;
+        $map = array(
+            '天' => $d,
+            '时' => $h,
+            '分' => $m,
+            '秒' => $s,
+        );
+        $result = '';
+        foreach ($map as $k => $v) {
+            if ($v) {
+                $result .= $v . $k;
+            }
+        }
+        return $result;
     }
 
     /**
@@ -116,9 +166,36 @@ class Tool
         // 用户传入的身份证的最后一位值
         $lastFromUser = substr($idCard, -1);
         if ($lastNum == $lastFromUser) {
-            return $idCard;
+            return true;
         }
         return false;
+    }
+
+    /**
+     * ajaxMessage
+     *
+     * @param  mixed $code 0:正常 大于0 都是非正常
+     * @param  mixed $msg
+     * @param  array $result
+     * @param  bool $isJsonp 是否使用 jsonp 方式提交数据
+     * @param  string $param jsonp 方式提交参数名
+     */
+    public function ajaxMessage($code = 0, $msg = 'success', $result = array(), $isJsonp = false, $param = 'jsoncallback')
+    {
+        if ($isJsonp) {
+            $jsonData = $this->json_encode2(array(
+                'code' => $code,
+                'msg' => $msg,
+                'result' => $result,
+            ));
+            echo $param . '(' . $jsonData . ')';
+        } else {
+            echo $this->json_encode2(array(
+                'code' => $code,
+                'msg' => $msg,
+                'result' => $result,
+            ));
+        }
     }
 
     /**
